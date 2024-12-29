e1b3c301692af2bf3d3dae8c0644d49649ba5ed0
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-12-28 22:12:53 -0500

    update 2

--- src/main/java/com/bandw/registry/ModBlocks.java
+++ src/main/java/com/bandw/registry/ModBlocks.java
@@ -8,8 +8,10 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
     public static final Block DARK_BRICKS = new blocks.DarkBricksBlock();
     public static final Block LIGHT_BRICKS = new blocks.LightBricksBlock();
+    public static final Block DARK_LOG = new blocks.DarkLogBlock();
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier("bandw", "dark_bricks"), DARK_BRICKS);
         Registry.register(Registry.BLOCK, new Identifier("bandw", "light_bricks"), LIGHT_BRICKS);
+        Registry.register(Registry.BLOCK, new Identifier("bandw", "dark_log"), DARK_LOG);
     };
 };
