5a7549db9e54b046f81543bf4cd6652c9e4f03c1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-12-28 21:17:46 -0500

    Update Main.java

--- src/main/java/com/bandw/Main.java
+++ src/main/java/com/bandw/Main.java
@@ -7,5 +7,18 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 // mod checker utility
 public class ModChecker { public static boolean isModLoaded(String modId) { return FabricLoader.getInstance().isModLoaded(modId);};};
-public class Main implements ModInitializer {public static boolean isCharterLoaded;@Overridepublic void onInitialize() {ModEntities.registerEntities();ModEntityAttributes.registerAttributes();ModItems.registerItems();isCharterLoaded = ModChecker.isModLoaded("charter");if (isCharterLoaded) {System.out.println("Charter mod is loaded! Enabling additional features.");}else{ System.out.println("Charter mod is not loaded.");};};
+public class Main implements ModInitializer {
+    public static boolean isCharterLoaded;
+    @Override
+    public void onInitialize() {
+        ModEntities.registerEntities();
+        ModEntityAttributes.registerAttributes();
+        ModItems.registerItems();
+        isCharterLoaded = ModChecker.isModLoaded("charter");
+        if (isCharterLoaded) {
+            System.out.println("Charter mod is loaded! Enabling additional features.");
+        }else{
+            System.out.println("Charter mod is not loaded.");
+        };
+    };
 };
