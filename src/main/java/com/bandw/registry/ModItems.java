766da5c77fc2e7717fd1b58a3889007c3ffb20b5
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-12-28 21:44:34 -0500

    Update ModItems.java

--- src/main/java/com/bandw/registry/ModItems.java
+++ src/main/java/com/bandw/registry/ModItems.java
@@ -5,11 +5,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 public class ModItems {
     public static final Item DARK_ENDERMAN_SPAWN_EGG = new SpawnEggItem(ModEntities.DARK_ENDERMAN, 0x000000, 0xFFFFFF, new FabricItemSettings().group(ItemGroup.MISC));
-
+    public static final Item DARK_BRICKS_ITEM = new BlockItem(ModBlocks.DARK_BRICKS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("bandw", "dark_enderman_spawn_egg"), DARK_ENDERMAN_SPAWN_EGG);
+        Registry.register(Registry.ITEM, new Identifier("bandw", "dark_bricks"), DARK_BRICKS_ITEM);
     }
 };
