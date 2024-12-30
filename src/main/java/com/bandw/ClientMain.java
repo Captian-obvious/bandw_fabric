87ad57bf9d0d9da944fe26e982ddb854596208ed
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-12-29 20:53:43 -0500

    add test class for electrical arcs (weapon coding time)

--- src/main/java/com/bandw/ClientMain.java
+++ src/main/java/com/bandw/ClientMain.java
@@ -2,12 +2,20 @@ package com.bandw;
 
 import com.bandw.registry.ModEntities;
 import com.bandw.render.DarkEndermanRenderer;
+import com.bandw.utils.ElectricArcUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.util.math.Vec3d;
 
 public class BandWModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.DARK_ENDERMAN, DarkEndermanRenderer::new);
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.world != null) {
+            Vec3d start = new Vec3d(0, 64, 0);
+            Vec3d end = new Vec3d(10, 64, 10);
+            ElectricArcUtils.createElectricArc(client.world, start, end, 0.5f, 1.0f);
+        };
     };
 };
