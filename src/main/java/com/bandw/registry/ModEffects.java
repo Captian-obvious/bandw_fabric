68554c249bbe3ccf2ffb0171ba4973fec144ff88
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-01-08 13:47:05 -0500

    fix registration of HARMFUL effect

--- src/main/java/com/bandw/registry/ModEffects.java
+++ src/main/java/com/bandw/registry/ModEffects.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModEffects {
-    public static final StatusEffect CORRUPTION_EFFECT = new effects.CorruptionStatusEffect(StatusEffectCategory.BENEFICIAL, 0x98D982);
+    public static final StatusEffect CORRUPTION_EFFECT = new effects.CorruptionStatusEffect(StatusEffectCategory.HARMFUL, 0x98D982);
     public static void registerEffects() {
         Registry.register(Registry.STATUS_EFFECT, new Identifier("bandw", "corruption"), CORRUPTION_EFFECT);
     };
--- src/main/java/com/bandw/registry/ModPotions.java
+++ src/main/java/com/bandw/registry/ModPotions.java
@@ -5,8 +5,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModPotions {
-    public static final Potion CORRUPTION_POTION = new Potion(new StatusEffectInstance(ModEffects.CUSTOM_EFFECT,3600));
-
+    public static final Potion CORRUPTION_POTION = new Potion(new StatusEffectInstance(ModEffects.CORRUPTION_EFFECT,3600));
     public static void registerPotions() {
         Registry.register(Registry.POTION, new Identifier("bandw", "corruption_potion"), CORRUPTION_POTION);
     };
