e009a0a5d2d6c64f65fe7bc5eec980cdf3339795
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-01-08 12:43:47 -0500

    register shield of light block entity renderer

--- src/main/java/com/bandw/ClientMain.java
+++ src/main/java/com/bandw/ClientMain.java
@@ -2,19 +2,19 @@ package com.bandw;
 
 import com.bandw.Main;
 import com.bandw.render;
-import com.bandw.registry.ModEntities;
-import com.bandw.render.DarkEndermanRenderer;
+import com.bandw.registry;
 import com.bandw.utils.ElectricArcUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.Vec3d;
 
 public class ClientMain implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(ModEntities.DARK_ENDERMAN, DarkEndermanRenderer::new);
-        BlockEntityRendererRegistry.INSTANCE.register(Main.SHIELD_BLOCK_ENTITY, render.ShieldRenderer::new);
+        EntityRendererRegistry.register(registry.ModEntities.DARK_ENDERMAN, render.DarkEndermanRenderer::new);
+        BlockEntityRendererRegistry.INSTANCE.register(registry.ModBlocks.SHIELD_OF_LIGHT_BLOCK_ENTITY, render.ShieldRenderer::new);
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.world != null) {
             Vec3d start = new Vec3d(0, 64, 0);
