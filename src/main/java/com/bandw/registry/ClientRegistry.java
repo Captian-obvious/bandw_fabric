7b0032a6727a44113c5c71b0b421131dd3ab3674
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-01-08 14:30:19 -0500

    add effect basics

--- src/main/java/com/bandw/registry/ClientRegistry.java
+++ src/main/java/com/bandw/registry/ClientRegistry.java
@@ -2,8 +2,14 @@ package com.bandw.registry;
 
 import com.bandw.render.DarkEndermanRenderer;
 import com.bandw.render.ShieldRenderer;
+import com.bandw.effects.client.CorruptionStatusEffectRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.Identifier;
 
 public class ClientRegistry {
     public static void registerClientRenderers() {
@@ -12,5 +18,7 @@ public class ClientRegistry {
 
         // Register block entity renderer
         BlockEntityRendererRegistry.INSTANCE.register(ClientReferences.SHIELD_OF_LIGHT_BLOCK_ENTITY, ShieldRenderer::new);
+
+        // Register custom status effect renderer
     };
 };
