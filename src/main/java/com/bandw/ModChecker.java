491c343dc2003bd483b4140c2f3d835c84f73727
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-01-10 08:09:35 -0500

    Update ModChecker.java

--- src/main/java/com/bandw/ModChecker.java
+++ src/main/java/com/bandw/ModChecker.java
@@ -1,5 +1,7 @@
 package com.bandw;
 
+import net.fabricmc.loader.api.FabricLoader;
+//mod checker utility
 public class ModChecker { 
     public static boolean isModLoaded(String modId) {
         return FabricLoader.getInstance().isModLoaded(modId);
