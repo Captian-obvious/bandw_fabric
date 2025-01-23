c00e50896227cac368e9c3de44316df8ae6e6202
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-01-23 11:53:04 -0500

    update mappings

--- src/main/java/com/bandw/registry/ModPotions.java
+++ src/main/java/com/bandw/registry/ModPotions.java
@@ -1,5 +1,6 @@
 package com.bandw.registry;
 
+import com.bandw.Main;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
@@ -9,6 +10,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 public class ModPotions {
     public static final Potion CORRUPTION_POTION = new Potion(new StatusEffectInstance(ModEffects.CORRUPTION_EFFECT,3600));
     public static void registerPotions() {
-        Registry.register(Registries.POTION, Identifier.of("bandw", "corruption_potion"), CORRUPTION_POTION);
+        Registry.register(Registries.POTION, Identifier.of(Main.MOD_ID, "corruption_potion"), CORRUPTION_POTION);
     };
 };
