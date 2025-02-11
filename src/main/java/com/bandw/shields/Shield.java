b4e1bc8cdc7b67f59712282eb99bd19cdd9be38e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-10 21:07:13 -0500

    missing imports corrected in server jar

---
+++ src/main/java/com/bandw/shields/Shield.java
@@ -0,0 +1,68 @@
+package com.bandw.shields;
+
+import net.minecraft.util.math.Vec3d;
+
+public class Shield {
+    private Vec3d position;
+    private float size;
+    private float strength;
+    private boolean isActive;
+
+    public Shield(Vec3d position, float size, float strength) {
+        this.position = position;
+        this.size = size;
+        this.strength = strength;
+        this.isActive = true;
+    };
+    public void expand(float amount) {
+        if (isActive) {
+            size += amount;
+        };
+    };
+    public void contract(float amount) {
+        if (isActive) {
+            size = Math.max(0, size - amount);
+        };
+    };
+    public void weaken(float amount) {
+        if (isActive) {
+            strength = Math.max(0, strength - amount);
+            if (strength<=0) {
+                this.collapse();
+            };
+        };
+    };
+    public void collapse(){
+        new Thread(()->{
+            while (size>0) {
+                size-=size*.05;
+                try{
+                    Thread.sleep(50);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                };
+            };
+        }).start();
+    };
+    public void toggleActive() {
+        isActive = !isActive;
+    };
+    public boolean getIsActive() {
+        return isActive;
+    };
+    public float getSize() {
+        return size;
+    };
+    public float getStrength() {
+        return strength;
+    };
+    public void setIsActive(boolean val) {
+        isActive=val;
+    };
+    public void setSize(int val) {
+        size=val;
+    };
+    public void setStrength(int val) {
+        strength=val;
+    };
+};
