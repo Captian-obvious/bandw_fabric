521e276366e538796dd2966566f13ca398c8b3c9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-11 11:44:22 -0500

    removal of corruption potion (not required)

--- src/main/java/com/bandw/effects/CorruptionStatusEffect.java
+++ src/main/java/com/bandw/effects/CorruptionStatusEffect.java
@@ -8,19 +8,15 @@ import net.minecraft.world.World;
 
 public class CorruptionStatusEffect extends StatusEffect {
     public CorruptionStatusEffect(StatusEffectCategory category, int color) {
-        super(category, color);
+        super(category,color);
     };
     // Override methods to define the custom effect behavior
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
-        // Define when the effect should be applied
-        return true;
+        return super.canApplyUpdateEffect(duration, amplifier);
     };
     @Override
-    public void applyUpdateEffect(World world, LivingEntity entity, int amplifier) {
-        // Define the effect on the entity
-        if (entity instanceof PlayerEntity) {
-            ((PlayerEntity) entity).addExperience(1); // Example: Grant experience points
-        };
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        super.applyUpdateEffect(entity, amplifier);
     };
 };
