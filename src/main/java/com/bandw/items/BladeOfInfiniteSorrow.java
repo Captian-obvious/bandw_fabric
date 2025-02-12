e0f7ba9d93483adf881aaed89a9770398bc8d685
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-12 12:24:27 -0500

    add various textures

--- src/main/java/com/bandw/items/BladeOfInfiniteSorrow.java
+++ src/main/java/com/bandw/items/BladeOfInfiniteSorrow.java
@@ -2,10 +2,7 @@ package com.bandw.items;
 
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
-
-/*public static final ToolMaterial GUIDITE_TOOL_MATERIAL = new ToolMaterial(
-    BlockTags.INCORRECT_FOR_WOODEN_TOOL,455,5.0F,1.5F,22,GuiditeArmorMaterial.REPAIRS_GUIDITE_ARMOR
-);*/
+import net.minecraft.item.Item.Settings;
 
 public class BladeOfInfiniteSorrow extends SwordItem {
     public BladeOfInfiniteSorrow(ToolMaterial material,int attackDamage,float attackSpeed,Settings settings){
--- src/main/java/com/bandw/registry/ModItems.java
+++ src/main/java/com/bandw/registry/ModItems.java
@@ -2,20 +2,27 @@ package com.bandw.registry;
 
 // Imports go below here
 import com.bandw.Main;
+import com.bandw.items.*;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    //public static final ToolMaterial INFINITE_SORROW_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL,455,5.0F,1.5F,22);
+    public static final Item light_shard=new Item(new Item.Settings().maxCount(64));
     public static final Item burnt_flesh=new Item(new Item.Settings().maxCount(64));
     public static final Item dark_bone=new Item(new Item.Settings().maxCount(64));
+    public static final Item dark_shard=new Item(new Item.Settings().maxCount(64));
     public static final Item dark_spider_eye=new Item(new Item.Settings().maxCount(63));
+    //public static final SwordItem blade_of_infinite_sorrow=new BladeOfInfiniteSorrow(INFINITE_SORROW_MATERIAL,);
     public void initialize(){
         System.out.println("Mod Items Initialized!");
         registerItems();
@@ -25,8 +32,10 @@ public class ModItems {
         return Registry.register(Registries.ITEM,key.getValue(),item);
     };
     public static void registerItems(){
+        register(light_shard,"light_shard");
         register(burnt_flesh,"dark_rotten_flesh");
         register(dark_bone,"dark_bone");
         register(dark_spider_eye,"dark_spider_eye");
+        register(dark_shard,"dark_shard");
     };
 };
