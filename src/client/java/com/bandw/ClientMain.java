672344a871069e393aa7bba8aca03808c8ce3f48
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-13 13:54:40 -0500

    Update ClientMain.java

--- src/client/java/com/bandw/ClientMain.java
+++ src/client/java/com/bandw/ClientMain.java
@@ -1,10 +1,8 @@
-package com.example;
-
+package com.bandw;
 import net.fabricmc.api.ClientModInitializer;
-
 public class ClientMain implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    };
+};
