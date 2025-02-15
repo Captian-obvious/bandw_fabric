fb9edd2576cf1d23542013848e041df2f19ecf90
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-15 17:10:29 -0500

    update all items to use registry keys

--- src/main/java/com/bandw/registry/ModItems.java
+++ src/main/java/com/bandw/registry/ModItems.java
@@ -18,26 +18,31 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     //public static final ToolMaterial INFINITE_SORROW_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_WOODEN_TOOL,455,5.0F,1.5F,22);
-    public static final Item light_shard=new Item(new Item.Settings().maxCount(64));
-    public static final Item burnt_flesh=new Item(new Item.Settings().maxCount(64));
-    public static final Item dark_bone=new Item(new Item.Settings().maxCount(64));
-    public static final Item dark_shard=new Item(new Item.Settings().maxCount(64));
-    public static final Item dark_spider_eye=new Item(new Item.Settings().maxCount(63));
-    public static final SwordItem blade_of_infinite_sorrow=new BladeOfInfiniteSorrow(ToolMaterial.DIAMOND,15,-2.4F,new Item.Settings());
+    RegistryKey<Item> light_shard_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"light_shard"));
+    public static final Item light_shard=new Item(new Item.Settings().maxCount(64).registryKey(light_shard_key));
+    RegistryKey<Item> burnt_flesh_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"dark_rotten_flesh"));
+    public static final Item burnt_flesh=new Item(new Item.Settings().maxCount(64).registryKey(burnt_flesh_key));
+    RegistryKey<Item> dark_bone_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"dark_bone"));
+    public static final Item dark_bone=new Item(new Item.Settings().maxCount(64).registryKey(dark_bone_key));
+    RegistryKey<Item> dark_shard_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"dark_shard"));
+    public static final Item dark_shard=new Item(new Item.Settings().maxCount(64).registryKey(dark_shard_key));
+    RegistryKey<Item> dark_spider_eye_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"dark_spider_eye"));
+    public static final Item dark_spider_eye=new Item(new Item.Settings().maxCount(63).registryKey(dark_spider_eye_key));
+    RegistryKey<Item> blade_of_infinite_sorrow_key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,"blade_of_infinite_sorrow"));
+    public static final SwordItem blade_of_infinite_sorrow=new BladeOfInfiniteSorrow(ToolMaterial.DIAMOND,15,-2.4F,new Item.Settings().registryKey(blade_of_infinite_sorrow_key));
     public void initialize(){
         System.out.println("Mod Items Initialized!");
         registerItems();
     };
-    public static Item register(Item item,String id){
-        RegistryKey<Item> key=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Main.MOD_ID,id));
-        return Registry.register(Registries.ITEM,key.getValue(),item);
+    public static Item register(Item item,RegistryKey key){
+        return Registry.register(Registries.ITEM,key,item);
     };
     public static void registerItems(){
-        register(blade_of_infinite_sorrow,"blade_of_infinite_sorrow");
-        register(light_shard,"light_shard");
-        register(burnt_flesh,"dark_rotten_flesh");
-        register(dark_bone,"dark_bone");
-        register(dark_spider_eye,"dark_spider_eye");
-        register(dark_shard,"dark_shard");
+        register(blade_of_infinite_sorrow,blade_of_infinite_sorrow_key);
+        register(light_shard,light_shard_key);
+        register(burnt_flesh,burnt_flesh_key);
+        register(dark_bone,dark_bone_key);
+        register(dark_spider_eye,dark_spider_eye_key);
+        register(dark_shard,dark_shard_key);
     };
 };
