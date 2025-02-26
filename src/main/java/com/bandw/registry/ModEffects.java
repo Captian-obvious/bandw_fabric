472c0bf2da9f4908fac8d1f893b4d5044ab9ddab
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-26 12:27:24 -0500

    fix food

--- src/main/java/com/bandw/registry/ModEffects.java
+++ src/main/java/com/bandw/registry/ModEffects.java
@@ -7,10 +7,12 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public class ModEffects {
     public static final StatusEffect CORRUPTION_EFFECT = new CorruptionStatusEffect(StatusEffectCategory.HARMFUL, 0x015032);
+    public static RegistryEntry<StatusEffect> CORRUPTION_INTERNAL;
     public static void registerEffects() {
-        Registry.register(Registries.STATUS_EFFECT, Identifier.of(Main.MOD_ID, "corruption"), CORRUPTION_EFFECT);
+        CORRUPTION_INTERNAL=Registry.register(Registries.STATUS_EFFECT, Identifier.of(Main.MOD_ID, "corruption"), CORRUPTION_EFFECT);
     };
 };
