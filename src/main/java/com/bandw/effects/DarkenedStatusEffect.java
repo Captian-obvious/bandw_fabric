258484ec69d5018cbf74866cc6c0aeac057e4cac
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-02-27 12:18:02 -0500

    most annoying fix part 2

--- src/main/java/com/bandw/effects/DarkenedStatusEffect.java
+++ src/main/java/com/bandw/effects/DarkenedStatusEffect.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
 
 public class DarkenedStatusEffect extends StatusEffect {
