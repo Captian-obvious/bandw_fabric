cbfc6dd8b91d699898a14ced8975b25b621a4429
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-06 11:09:37 -0500

    not fixed at all

--- src/main/java/com/bandw/registry/ModBlockEntities.java
+++ src/main/java/com/bandw/registry/ModBlockEntities.java
@@ -14,6 +14,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.block.AbstractBlock.Settings;
 
