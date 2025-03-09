68cc498435e734165d2828c52bfece10465c32a9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-09 12:31:13 -0400

    fix client

--- src/client/java/com/bandw/render/ShieldBlockEntityRenderer.java
+++ src/client/java/com/bandw/render/ShieldBlockEntityRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
