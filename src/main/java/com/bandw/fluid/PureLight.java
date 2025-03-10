d03390ad03cc677cf79efd93d3239e4da999d449
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-10 11:25:16 -0400

    Begin to implement Pure Light (flowable fluid)

---
+++ src/main/java/com/bandw/fluid/PureLight.java
@@ -0,0 +1,12 @@
+package com.bandw.fluid;
+
+import net.minecraft.block.Block;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.block.AbstractBlock.Settings;
+
+public class PureLight extends FlowableFluid {
+    public PureLight(){
+        super();
+    };
+};
