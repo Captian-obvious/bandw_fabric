5a981179306c4856aafb3d4e17189daaf9c2557e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-10 11:31:42 -0400

    start implementing fluid

--- src/main/java/com/bandw/fluid/PureLight.java
+++ src/main/java/com/bandw/fluid/PureLight.java
@@ -1,12 +1,32 @@
 package com.bandw.fluid;
 
 import net.minecraft.block.Block;
-import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.block.AbstractBlock.Settings;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 
-public class PureLight extends FlowableFluid {
-    public PureLight(){
-        super();
+public abstract class PureLight extends FlowableFluid {
+    @Override
+    protected void appendProperties(StateManager.Builder<Fluid, FluidState> builder) {
+        super.appendProperties(builder);
+        builder.add(Properties.LEVEL_1_8); // Add the fluid level property
+    };
+    @Override
+    public boolean matchesType(Fluid fluid) {
+        return fluid == STILL || fluid == FLOWING;
+    };
+    @Override
+    protected boolean isInfinite() {
+        return false; // Define if the fluid source is infinite
+    };
+    @Override
+    protected void beforeBreakingBlock(BlockView world, BlockPos pos, FluidState state) {
+        // Custom behavior when breaking a block
     };
 };
