99c4833cab4cc242c0065d9d1c5944d7dedd8253
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-10 10:53:15 -0400

    add cleansing

--- src/main/java/com/bandw/registry/ModEffects.java
+++ src/main/java/com/bandw/registry/ModEffects.java
@@ -12,10 +12,13 @@ import net.minecraft.registry.entry.RegistryEntry;
 public class ModEffects {
     public static final StatusEffect CORRUPTION_EFFECT = new CorruptionStatusEffect(StatusEffectCategory.HARMFUL, 0x015032);
     public static final StatusEffect DARKENED_EFFECT = new DarkeningStatusEffect(StatusEffectCategory.HARMFUL, 0x111111);
+    public static final StatusEffect CLEANSING_EFFECT = new CleansingStatusEffect(StatusEffectCategory.BENEFICIAL, 0x111111);
     public static RegistryEntry<StatusEffect> CORRUPTION;
     public static RegistryEntry<StatusEffect> DARKENING;
+    public static RegistryEntry<StatusEffect> CLEANSING;
     public static void registerEffects() {
         CORRUPTION=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Main.MOD_ID, "corruption"), CORRUPTION_EFFECT);
         DARKENING=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Main.MOD_ID, "darkening"), DARKENED_EFFECT);
+        CLEANSING=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Main.MOD_ID, "cleansing"), CLEANSING_EFFECT);
     };
 };
