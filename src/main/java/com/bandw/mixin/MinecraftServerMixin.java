7642dfd3643a8da2d6dbd46e05e1fe56d42dd988
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-14 11:38:10 -0400

    fix mixin again

--- src/main/java/com/bandw/mixin/MinecraftServerMixin.java
+++ src/main/java/com/bandw/mixin/MinecraftServerMixin.java
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin{
-	@Inject(at=@At("HEAD"),method="loadWorld")
-	private void init(CallbackInfo info){
-		Main.LOGGER.info("Loading world...");
-	};
+    @Inject(at=@At("HEAD"),method="loadWorld")
+    private void init(CallbackInfo info){
+        Main.LOGGER.info("Loading world...");
+    };
 };
