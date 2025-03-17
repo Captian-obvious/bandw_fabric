745a88fe605d23f6825a9b85b864c168b000fab9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-03-17 11:48:18 -0400

    fixed attributes

--- src/main/java/com/bandw/registry/ModEntityAttributes.java
+++ src/main/java/com/bandw/registry/ModEntityAttributes.java
@@ -1,5 +1,6 @@
 package com.bandw.registry;
 
+import com.bandw.Main;
 import com.bandw.entities.*;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -7,6 +8,7 @@ import net.minecraft.entity.attribute.EntityAttributes;
 
 public class ModEntityAttributes {
     public static void registerAttributes() {
+        Main.LOGGER.info("Registering Entity Attributes...")
         FabricDefaultAttributeRegistry.register(ModEntities.DARK_ENDERMAN,DarkEndermanEntity.createEndermanAttributes());
     };
 };
