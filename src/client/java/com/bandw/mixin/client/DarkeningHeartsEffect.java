4296fd2d3d70c50ffd0f478b8390b6012ce2c9bb
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-09 10:59:01 -0400

    fix mixin

--- src/client/java/com/bandw/mixin/client/DarkeningHeartsEffect.java
+++ src/client/java/com/bandw/mixin/client/DarkeningHeartsEffect.java
@@ -4,7 +4,7 @@ import com.bandw.registry.ModEffects;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,8 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class DarkeningHeartsEffect {
     @Inject(method = "renderHealthBar", at = @At("HEAD"), cancellable = true)
-    private void onRenderHealthBar(MatrixStack matrices, CallbackInfo info) {
-        PlayerEntity player = MinecraftClient.getInstance().player;
+    private void onRenderHealthBar(DrawContext context,PlayerEntity player,int x,int y,int lines,int regeneratingHeartIndex,float maxHealth,int lastHealth,int health,int absorption,boolean blinking,CallbackInfo info) {
         if (player != null && player.hasStatusEffect(ModEffects.DARKENING)) {
             // Custom rendering logic to make the health bar black
             RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, 1.0F); // Set to black
