19140b16fff282368198fe350f41c892a1531e73
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-11 12:37:03 -0400

    fix static context (maybe)

--- src/main/java/com/bandw/util/CorruptionManager.java
+++ src/main/java/com/bandw/util/CorruptionManager.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CorruptionManager {
-    private final Map<Block, Block> replacementMap;
+    private static final Map<Block, Block> replacementMap;
     
     public CorruptionManager() {
         this.replacementMap = new HashMap<>();
     };
-    public void addReplacement(Block original, Block corrupted) {
+    public static void addReplacement(Block original, Block corrupted) {
         replacementMap.put(original,corrupted);
     };
-    public Block getReplacement(Block original) {
+    public static Block getReplacement(Block original) {
         return replacementMap.getOrDefault(original,original); // Return the original if no replacement is found
     };
 };
