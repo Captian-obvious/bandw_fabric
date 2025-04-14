5365a03b49d411ad998849ccdc64ba9132eb244b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-14 10:52:21 -0400

    mod version 2.7.4 (fr this time)

--- src/client/java/com/bandw/ClientMain.java
+++ src/client/java/com/bandw/ClientMain.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class ClientMain implements ClientModInitializer {
     public static final String MOD_ID = "bandw";
-    public static final String VERSION="1.9.5";
+    public static final String VERSION="2.7.4";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitializeClient() {
--- src/main/java/com/bandw/Main.java
+++ src/main/java/com/bandw/Main.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class Main implements ModInitializer {
     public static boolean isCharterLoaded;
     public static final String MOD_ID = "bandw";
-    public static final String VERSION="1.9.5";
+    public static final String VERSION="2.7.4";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static boolean judgmentEnabled=false;
     @Override
