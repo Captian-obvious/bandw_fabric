9261c81ec9241710929c764c080f0071e0f4662e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-14 09:47:07 -0400

    make halfshade blade always apply effect

--- src/main/java/com/bandw/items/HalfshadeBlade.java
+++ src/main/java/com/bandw/items/HalfshadeBlade.java
@@ -2,7 +2,7 @@ package com.bandw.items;
 
 import com.bandw.Main;
 import com.bandw.registry.ModEffects;
-import com.bandw.items.SwordItemWithEffect;
+import net.minecraft.item.SwordItem;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ToolMaterial;
@@ -16,20 +16,23 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-public class HalfshadeBlade extends SwordItemWithEffect {
+public class HalfshadeBlade extends SwordItem {
     public HalfshadeBlade(ToolMaterial material,int attackDamage,float attackSpeed,Settings settings){
         super(material,attackDamage,attackSpeed,settings);
     };
     @Override
-    public void weapon_effect(ItemStack stack, LivingEntity target, LivingEntity attacker){
-        if (target!=null && attacker!=null){
-            StatusEffectInstance instance = new StatusEffectInstance(ModEffects.HALFSHADE_POISONING,200,1,false,true,true);
-            boolean success=target.addStatusEffect(instance);
-            if (!success){
-                Main.LOGGER.info("Failed to apply effect 'bandw:halfshade_poisoning' to entity, ignoring and continuing");
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker){
+        if (!target.getWorld().isClient){
+            if (target!=null && attacker!=null){
+                StatusEffectInstance instance = new StatusEffectInstance(ModEffects.HALFSHADE_POISONING,200,1,false,true,true);
+                boolean success=target.addStatusEffect(instance);
+                if (!success){
+                    Main.LOGGER.info("Failed to apply effect 'bandw:halfshade_poisoning' to entity, ignoring and continuing");
+                };
+            }else{
+                Main.LOGGER.info("ERROR: target and attacker must not be null!");
             };
-        }else{
-            Main.LOGGER.info("ERROR: target and attacker must not be null!");
         };
+        return super.postHit(stack,target,attacker);
     };
 };
