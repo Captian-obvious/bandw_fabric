b6df442398c63c373d0fc2073cd3095fe06aec69
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-22 10:13:08 -0400

    correct missing import

--- src/main/java/com/bandw/item/Casshen.java
+++ src/main/java/com/bandw/item/Casshen.java
@@ -4,6 +4,7 @@ import com.bandw.Main;
 import com.bandw.registry.ModComponents;
 import com.bandw.item.SwordItemWithEffect;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.item.Item.Settings;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.EntityType;
