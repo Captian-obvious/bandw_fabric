c2a07f3cc1d1636208688dc89ed097c87e2a2d59
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-23 11:16:37 -0400

    mod version 2.7.5

--- src/main/java/com/bandw/Main.java
+++ src/main/java/com/bandw/Main.java
@@ -22,7 +22,7 @@ public class Main implements ModInitializer {
     public static boolean isCharterLoaded;
     public static CharterContractManager contract_manager;
     public static final String MOD_ID = "bandw";
-    public static final String VERSION="2.7.4";
+    public static final String VERSION="2.7.5";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static boolean judgmentEnabled=false;
     @Override
