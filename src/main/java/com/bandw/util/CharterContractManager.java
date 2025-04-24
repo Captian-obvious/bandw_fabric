a24fb3bae87a37dab2b133d49b4ca22e3d00cedf
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-24 10:19:06 -0400

    (PREFERABLY) add initialization to Charter Contract Manager

--- src/main/java/com/bandw/util/CharterContractManager.java
+++ src/main/java/com/bandw/util/CharterContractManager.java
@@ -5,7 +5,16 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class CharterContractManager {
+    public boolean isInitialized=false;
+    private Map<Integer,String> contracts=new HashMap<>();
     public CharterContractManager(boolean isCharterLoaded){
-        Main.LOGGER.info(isCharterLoaded ? "Charter is loaded, setting up Contract hooks..." : "Charter is not loaded, Contract severing functionality not available.");
+        Main.LOGGER.info(isCharterLoaded && this.initialize() ? "Charter is loaded, setting up Contract hooks..." : "Charter is not loaded, Contract severing functionality not available.");
+    };
+    public boolean initialize(){
+        if (!isInitialized){
+            isInitialized=true;
+            Main.LOGGER.info("ChartMan: initialize()");
+        };
+        return true;
     };
 };
