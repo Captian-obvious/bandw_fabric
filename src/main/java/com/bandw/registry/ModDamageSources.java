ba2ea76acfdcaf8687b03ac2c845bc81ee006809
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-04-24 10:43:48 -0400

    fix indentation

--- src/main/java/com/bandw/registry/ModDamageSources.java
+++ src/main/java/com/bandw/registry/ModDamageSources.java
@@ -13,10 +13,10 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.entity.damage.DamageType;
 
 public class ModDamageSources {
-	public static final RegistryKey<DamageType> CASSHEN_EXPLOSION = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "casshen_explode"));
-	public static final RegistryKey<DamageType> CASSHEN_BEAM = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "casshen_beam"));
-	public static final RegistryKey<DamageType> FRACTURED = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "fractured"));
-	public static void registerDamageSources() {
-		Main.LOGGER.info("Registering damage types",Main.MOD_ID);
-	};
+    public static final RegistryKey<DamageType> CASSHEN_EXPLOSION = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "casshen_explode"));
+    public static final RegistryKey<DamageType> CASSHEN_BEAM = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "casshen_beam"));
+    public static final RegistryKey<DamageType> FRACTURED = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(Main.MOD_ID, "fractured"));
+    public static void registerDamageSources() {
+        Main.LOGGER.info("Registering damage types",Main.MOD_ID);
+    };
 };
