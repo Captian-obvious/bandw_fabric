d06867284533ef0f4458ca6dc4b351d0899b224b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-09 10:32:01 -0400

    h2

--- src/main/java/com/normalorg/bandw/mixin/MinecraftServerMixin.java
+++ src/main/java/com/normalorg/bandw/mixin/MinecraftServerMixin.java
@@ -2,6 +2,7 @@ package com.normalorg.bandw.mixin;
 
 import com.normalorg.bandw.Defiance;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftServerMixin{
     @Inject(at=@At("HEAD"),method="loadWorld")
     private void init(CallbackInfo info){
+        Defiance.LOGGER.info("Loading dependancies...");
+        // some world initialization tasks
+        if (Defiance.isCharterLoaded && Defiance.LESSER_DIVINITY!=null){
+            Defiance.LOGGER.info("Initializing Lesser Divinity effects");
+            if (Defiance.LESSER_DIVINITY instanceof Item){
+                Defiance.LOGGER.info("Enabling Lesser Divinity cleansing effect");
+                // some basic initialization tasks as well as cleasning functionality
+            };
+        };
+        // Log completion, start loading
         Defiance.LOGGER.info("Loading world...");
     };
 };
