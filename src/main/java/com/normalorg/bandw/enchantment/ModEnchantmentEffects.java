2b060fd9524b4f019ab7425eed21b8d0feff14fb
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-09 14:29:52 -0400

    h2

---
+++ src/main/java/com/normalorg/bandw/enchantment/ModEnchantmentEffects.java
@@ -0,0 +1,27 @@
+package com.normalorg.bandw.enchantment;
+
+import com.normalorg.bandw.Defiance;
+import com.normalorg.bandw.enchantment.effect.SunderbeamEnchantmentEffect
+import com.mojang.serialization.MapCodec;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.effect.EnchantmentEntityEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModEnchantmentEffects {
+    public static final RegistryKey<Enchantment> SUNDERBEAM=of("sunderbeam");
+    public static MapCodec<SunderbeamEnchantmentEffect> SUNDERBEAM_EFFECT=register("sunderbeam_effect", SunderbeamEnchantmentEffect.CODEC);
+    private static RegistryKey<Enchantment> of(String path) {
+		Identifier id = Identifier.of(Defiance.MOD_ID,path);
+		return RegistryKey.of(RegistryKeys.ENCHANTMENT,id);
+	};
+    private static <T extends EnchantmentEntityEffect> MapCodec<T> register(String id, MapCodec<T> codec) {
+        return Registry.register(Registries.ENCHANTMENT_ENTITY_EFFECT_TYPE,Identifier.of(Defiance.MOD_ID, id),codec);
+	};
+    public static void registerEnchantmentEffects(){
+        Defiance.LOGGER.info("Registering EnchantmentEffects...")
+    };
+};
---
+++ src/main/java/com/normalorg/bandw/enchantment/effect/SunderbeamEnchantmentEffect.java
@@ -0,0 +1,27 @@
+package com.normalorg.bandw.enchantment.effect;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.enchantment.EnchantmentEffectContext;
+import net.minecraft.enchantment.EnchantmentLevelBasedValue;
+import net.minecraft.enchantment.effect.EnchantmentEntityEffect;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+public record SunderbeamEnchantmentEffect(EnchantmentLevelBasedValue amount) implements EnchantmentEntityEffect {
+	public static final MapCodec<SunderbeamEnchantmentEffect> CODEC=RecordCodecBuilder.mapCodec(instance ->instance.group(EnchantmentLevelBasedValue.CODEC.fieldOf("amount").forGetter(LightningEnchantmentEffect::amount)).apply(instance, SunderbeamEnchantmentEffect::new));
+    public void apply(ServerWorld world,int level,EnchantmentEffectContext context,Entity target,Vec3d pos){
+        if (target instanceof LivingEntity victim){
+
+        };
+    };
+    public MapCodec<? extends EnchantmentEntityEffect> getCodec() {
+        return CODEC;
+    };
+};
