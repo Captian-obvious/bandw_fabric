849988c2df50e4a222e70549974db2facaa32ed6
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-14 11:47:46 -0400

    reorder imports (readability)

--- src/client/java/com/normalorg/bandw/DefianceClient.java
+++ src/client/java/com/normalorg/bandw/DefianceClient.java
@@ -1,11 +1,12 @@
 package com.normalorg.bandw;
 
-import com.normalorg.bandw.render.entity.ModEntityRenderers;
-import com.normalorg.bandw.render.block.entity.ModBlockEntityRenderers;
-import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ClientModInitializer;
+import com.normalorg.bandw.render.entity.ModEntityRenderers;
+import com.normalorg.bandw.render.block.entity.ModBlockEntityRenderers;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+
 public class DefianceClient implements ClientModInitializer {
     public static final String MOD_ID = "bandw";
     public static final String VERSION="2.7.6";
