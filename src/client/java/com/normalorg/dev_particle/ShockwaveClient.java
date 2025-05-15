83508255783ab944ad4f11ecb1f9dd6fb66d1b27
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-15 10:39:13 -0400

    add Shockwave library officially

---
+++ src/client/java/com/normalorg/dev_particle/ShockwaveClient.java
@@ -0,0 +1,16 @@
+package com.normalorg.dev_particle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShockwaveClient implements ClientModInitializer {
+    public static final String MOD_ID = "shockwave";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitializeClient() {
+        // This is the client-side initialization method.
+        // You can register your client-side features here.
+        LOGGER.info("Client Library for Shockwave initialized.");
+    };
+};
---
+++ src/client/java/com/normalorg/dev_particle/api/ParticleHandler.java
@@ -0,0 +1,11 @@
+package com.normalorg.dev_particle.api;
+
+import net.minecraft.client.particle.Particle;
+
+public class ParticleHandler {
+    public static void doNothing() {
+        // This method is intentionally left blank.
+        // prevents java from erroring when class is compiled
+        // and the class is not used yet
+    };
+};
