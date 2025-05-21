c4689a10203e482cc1b6bf76de7c19c6d2216a4d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-21 13:17:59 -0400

    fix

--- src/client/java/com/normalorg/bandw/mixin/client/StatusEffectHearts.java
+++ src/client/java/com/normalorg/bandw/mixin/client/StatusEffectHearts.java
@@ -59,7 +59,7 @@ public abstract class StatusEffectHearts {
             for (int i = 0; i < (int) health_loop; i++) {
                 int heartX = x + (i % 10) * 8;
                 int heartY = y - (i / 10) * 10;
-                boolean halfHeart = ((i==health_loop) && ((health % 2) == 1);
+                boolean halfHeart = ((i==health_loop) && ((health % 2) == 1));
                 // draw the heart
                 ctx.drawTexture(RenderLayer::getGuiTextured,heartTexture,heartX,heartY,halfHeart ? 9.0F : 0.0F,0.0F,9,9,18,9);
             };
