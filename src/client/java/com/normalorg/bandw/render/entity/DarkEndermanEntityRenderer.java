ae79b44efc80dfa584eb7f8d44f926ad1660f4f5
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-25 18:59:02 -0400

    fix dark enderman texture

--- src/client/java/com/normalorg/bandw/render/entity/DarkEndermanEntityRenderer.java
+++ src/client/java/com/normalorg/bandw/render/entity/DarkEndermanEntityRenderer.java
@@ -3,11 +3,16 @@ package com.normalorg.bandw.render.entity;
 import com.normalorg.bandw.DefianceClient;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.EndermanEntityRenderer;
+import net.minecraft.client.render.entity.state.EndermanEntityRenderState;
 import net.minecraft.util.Identifier;
 
 public class DarkEndermanEntityRenderer extends EndermanEntityRenderer {
     private static final Identifier TEXTURE = Identifier.of(DefianceClient.MOD_ID, "textures/entity/dark_enderman.png");
     public DarkEndermanEntityRenderer(EntityRendererFactory.Context context) {
         super(context);
+    };
+    @Override
+    public Identifier getTexture(EndermanEntityRenderState state){
+        return TEXTURE;
     };
 };
