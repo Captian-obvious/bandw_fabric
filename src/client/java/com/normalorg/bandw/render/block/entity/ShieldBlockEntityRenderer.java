7d3a80ea3f55e12fd0c112632881eba7cfb3944f
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-05-25 19:12:38 -0400

    fix void bound block entity renderer

--- src/client/java/com/normalorg/bandw/render/block/entity/ShieldBlockEntityRenderer.java
+++ src/client/java/com/normalorg/bandw/render/block/entity/ShieldBlockEntityRenderer.java
@@ -30,7 +30,7 @@ public class ShieldBlockEntityRenderer implements BlockEntityRenderer<ShieldBloc
         double camY = MinecraftClient.getInstance().gameRenderer.getCamera().getPos().y;
         double camZ = MinecraftClient.getInstance().gameRenderer.getCamera().getPos().z;
         // do not render 200 blocks in the air this time please >:(
-        matrices.translate((pos.getX()), (pos.getY()), (pos.getZ()));
+        matrices.translate((pos.getX()+0.5f), (pos.getY()+0.5f), (pos.getZ()+0.5f));
         float size = MathHelper.clamp(entity.getShield().getSize(), 1.0F, 200.0F);
         matrices.scale(size, size, size);
         renderCube(matrices, vertexConsumers, light, overlay, TEXTURE, size);
