4b7d55094eee92d13bf6e5ef7514046359d21035
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-06-04 15:46:10 -0400

    fix

--- src/main/java/com/normalorg/bandw/item/tec/TecItems.java
+++ src/main/java/com/normalorg/bandw/item/tec/TecItems.java
@@ -31,7 +31,7 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class TecItems {
-    public static final Item TEC_DUST=register("tec_dust",Item::new,new Item.Settings().maxCount(64).registryKey(tec_dust_key))
+    public static final Item TEC_DUST=register("tec_dust",Item::new,new Item.Settings().maxCount(64))
     private static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         RegistryKey<Item> itemKey=RegistryKey.of(RegistryKeys.ITEM,Identifier.of(Defiance.MOD_ID,name));
         Item item=itemFactory.apply(settings.registryKey(itemKey));
