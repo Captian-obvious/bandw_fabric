d49a7f1274f678f95aa88ae2b2acaf4c6e2189fd
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-06-19 21:22:48 -0400

    changes

--- src/client/java/com/normalorg/bandw/mixin/client/StatusEffectHearts.java
+++ src/client/java/com/normalorg/bandw/mixin/client/StatusEffectHearts.java
@@ -40,7 +40,7 @@ public abstract class StatusEffectHearts {
             info.cancel();
         };
     };*/
-    @Inject(method = "renderHealthBar", at = @At("RETURN"))
+    @Inject(method = "renderHealthBar", at = @At("HEAD"), cancellable = true)
     private void onRenderHealthBar(DrawContext ctx,PlayerEntity player,int x,int y,int lines,int regeneratingHeartIndex,float maxHealth,int lastHealth,int health,int absorption,boolean blinking,CallbackInfo info) {
         if (player == null || !player.hasStatusEffect(ModEffects.DARKENING) && !player.hasStatusEffect(ModEffects.HALFSHADE_POISONING) && !player.hasStatusEffect(ModEffects.CRYSTAL_INFESTATION)) {
             // Default rendering logic (do nothing special)
@@ -75,6 +75,7 @@ public abstract class StatusEffectHearts {
                 // draw the heart
                 ctx.drawTexture(RenderLayer::getGuiTextured,heartTexture,heartX,heartY,halfHeart ? 9.0F : 0.0F,0.0F,9,9,hasCrystalInfestation ? 27 : 18,9);
             };
+            info.cancel(); // Cancel the default rendering logic
         };
     };
 };
