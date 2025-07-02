e2585fe32329b9e5c30a3b6a35a0d59843754cf4
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-02 09:27:29 -0400

    h

--- src/main/java/com/normalorg/bandw/item/PureDivinity.java
+++ src/main/java/com/normalorg/bandw/item/PureDivinity.java
@@ -57,6 +57,6 @@ public class PureDivinity extends Item {
     };
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type){
-        tooltip.add(Text.translatable("itemTooltip.bandw.pure_divinity").formatted(Formatting.GOLD))
+        tooltip.add(Text.translatable("itemTooltip.bandw.pure_divinity").formatted(Formatting.GOLD));
     };
 };
