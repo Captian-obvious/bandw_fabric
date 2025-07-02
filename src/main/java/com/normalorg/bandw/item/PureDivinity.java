496a0bfa257554d96002dd577967f9e4886fba25
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-02 10:06:24 -0400

    (this is likely to fail to build) test pure divinity

--- src/main/java/com/normalorg/bandw/item/PureDivinity.java
+++ src/main/java/com/normalorg/bandw/item/PureDivinity.java
@@ -24,7 +24,6 @@ import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.world.World;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.world.explosion.Explosion;
 import net.minecraft.util.math.BlockPos;
@@ -35,28 +34,32 @@ import net.minecraft.text.Text;
 import java.util.List;
 
 public class PureDivinity extends Item {
-    int effectDelay=20; //delay in ticks before applying effect
-    int effectAcc=0;
     public PureDivinity(Settings settings) {
         super(settings);
     };
     @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected){
-        if (entity instanceof LivingEntity livingEntity) {
-            effectAcc+=1;
-            if (!livingEntity.hasStatusEffect(StatusEffects.INVISIBILITY)){
-                // apply resistance to the player (they are immortal)
-                StatusEffectInstance instance = new StatusEffectInstance(StatusEffects.INVISIBILITY,100,5,false,true,true);
-                boolean success=livingEntity.addStatusEffect(instance);
-                if (!success){
-                    Defiance.LOGGER.info("Failed to apply effect 'minecraft:resistance' to entity, ignoring and continuing");
-                };
-                effectAcc=0;
-            };
-        };
-    };
-    @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type){
         tooltip.add(Text.translatable("itemTooltip.bandw.pure_divinity").formatted(Formatting.GOLD));
+    };
+    public boolean isEnabled() {
+        return true;
+    };
+    public void onKill(LivingEntity target, LivingEntity attacker) {
+        if (attacker.getWorld() instanceof ServerWorld world) {
+            target.setHealth(target.getMaxHealth());
+            new Thread(()->{
+                try{
+                    Thread.sleep(500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                };
+                DamageSource damageSource=new DamageSource(world.getRegistryManager().getOrThrow(RegistryKeys.DAMAGE_TYPE).getEntry(ModDamageSources.NONSEVERING_STRIKE.getValue()).get());
+                BlockPos pos=target.getBlockPos();
+                LightningEntity bolt=new LightningEntity(EntityType.LIGHTNING_BOLT,world);
+                bolt.refreshPositionAfterTeleport(Vec3d.ofBottomCenter(pos));
+                world.spawnEntity(bolt);
+                target.damage(world,damageSource,target.getMaxHealth());
+            }).start();
+        };
     };
 };
---
+++ src/main/java/com/normalorg/bandw/mixin/LivingEntityMixin.java
@@ -0,0 +1,39 @@
+package com.normalorg.bandw.mixin;
+
+import com.normalorg.bandw.Defiance;
+import com.normalorg.bandw.item.PureDivinity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin{
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void onDamage(ServerWorld world,DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if ((Object)this instanceof LivingEntity target && !target.getWorld().isClient) {
+            float resultingHealth = target.getHealth() - amount;
+            if (resultingHealth <= 0.0F && source.getAttacker() instanceof PlayerEntity attacker) {
+                ItemStack used = attacker.getMainHandStack();
+                if (!(used.getItem() instanceof PureDivinity)) {
+                    for (ItemStack stack : attacker.getInventory().main) {
+                        if (stack.getItem() instanceof PureDivinity pureDivinity) {
+                            if (pureDivinity.isEnabled()) {
+                                pureDivinity.onKill(attacker, target);
+                                break;
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+};
