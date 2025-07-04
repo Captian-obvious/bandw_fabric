263dc70796df5c2dd0ad3a0ded5ce18c72a0344b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-04 14:21:30 -0400

    add pure light bricks

--- src/main/java/com/normalorg/bandw/block/ModBlocks.java
+++ src/main/java/com/normalorg/bandw/block/ModBlocks.java
@@ -27,6 +27,7 @@ public class ModBlocks {
     public static final Block DEAD_DARK_LOG=register("dead_dark_log", PillarBlock::new, Settings.create().sounds(BlockSoundGroup.WOOD).strength(2.0F, 2.0F),true);
     public static final Block DARK_BARK=register("dark_bark", Block::new, Settings.create().sounds(BlockSoundGroup.WOOD).strength(2.0F, 2.0F),true);
     public static final Block LIGHT_BRICKS=register("light_bricks", Block::new, Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0F, 3.0F),true);
+    public static final Block PURE_LIGHT_BRICKS=register("pure_light_bricks", Block::new, Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0F, 3.0F),true);
     public static final Block LIGHT_CHISELED_BRICKS=register("light_chiseled_bricks", Block::new, Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0F, 3.0F),true);
     public static final Block CRACKED_LIGHT_BRICKS=register("cracked_light_bricks", Block::new, Settings.create().sounds(BlockSoundGroup.STONE).strength(3.0F, 3.0F),true);
     public static final Block LIGHT_PLANKS=register("light_planks", Block::new, Settings.create().sounds(BlockSoundGroup.WOOD).strength(2.0F, 2.0F),true);
