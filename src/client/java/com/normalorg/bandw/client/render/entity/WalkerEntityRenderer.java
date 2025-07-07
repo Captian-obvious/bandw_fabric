ab23e789397366bd555760b018c891e131b6c9f9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-06 21:52:05 -0400

    fix

--- src/client/java/com/normalorg/bandw/client/render/entity/WalkerEntityRenderer.java
+++ src/client/java/com/normalorg/bandw/client/render/entity/WalkerEntityRenderer.java
@@ -19,7 +19,7 @@ public class WalkerEntityRenderer extends MobEntityRenderer<WalkerEntity,WalkerE
         return TEXTURE;
     };
     @Override
-    protected WalkerEntityRenderState createRenderState(WalkerEntity entity, float tickDelta) {
+    protected WalkerEntityRenderState createRenderState() {
         return new WalkerEntityRenderState(); // You can flesh this out later
     };
 };
