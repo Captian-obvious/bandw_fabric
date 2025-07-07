2183cbbd741ef7fd6aa853cbd5aa4a97dbd76fe7
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-06 21:38:44 -0400

    try again

--- src/client/java/com/normalorg/bandw/client/render/entity/WalkerEntityRenderer.java
+++ src/client/java/com/normalorg/bandw/client/render/entity/WalkerEntityRenderer.java
@@ -1,7 +1,7 @@
 package com.normalorg.bandw.client.render.entity;
 
 import com.normalorg.bandw.client.DefianceClient;
-import com.normalorg.bandw.client.entity.model.ModModelLayers;
+import com.normalorg.bandw.client.render.entity.model.ModEntityModelLayers;
 import com.normalorg.bandw.client.render.entity.state.WalkerEntityRenderState;
 import com.normalorg.bandw.client.render.entity.model.WalkerEntityModel;
 import com.normalorg.bandw.entity.WalkerEntity;
@@ -12,7 +12,7 @@ import net.minecraft.util.Identifier;
 public class WalkerEntityRenderer extends MobEntityRenderer<WalkerEntity,WalkerEntityRenderState,WalkerEntityModel> {
     private static final Identifier TEXTURE = Identifier.of(DefianceClient.MOD_ID, "textures/entity/walker.png");
     public WalkerEntityRenderer(EntityRendererFactory.Context context) {
-        super(context,new WalkerEntityModel(context.getModelLoader().getModelPart(ModModelLayers.WALKER)),0.6f);
+        super(context,new WalkerEntityModel(context.getModelLoader().getModelPart(ModEntityModelLayers.WALKER)),0.6f);
     };
     @Override
     public Identifier getTexture(WalkerEntityRenderState state){
