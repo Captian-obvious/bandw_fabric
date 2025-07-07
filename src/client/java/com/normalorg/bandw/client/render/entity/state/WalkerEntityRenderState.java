e3a18160ce55ae1a6706473f88c8b30717d7696b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-06 20:48:35 -0400

    (maybe) add walker entity model

---
+++ src/client/java/com/normalorg/bandw/client/render/entity/state/WalkerEntityRenderState.java
@@ -0,0 +1,10 @@
+package com.normalorg.bandw.client.render.entity.state;
+
+import com.normalorg.bandw.client.DefianceClient;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+
+public class WalkerEntityRenderState extends LivingEntityRenderState {
+    public WalkerEntityRenderState() {
+        super();
+    };
+};
