7a741f93072f78e176360cf9632c35a60ddefeda
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-07-23 11:28:26 -0400

    probably fail

--- src/main/java/com/normalorg/bandw/entity/WalkerEntity.java
+++ src/main/java/com/normalorg/bandw/entity/WalkerEntity.java
@@ -3,6 +3,11 @@ package com.normalorg.bandw.entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
@@ -17,5 +22,12 @@ public class WalkerEntity extends PathAwareEntity {
             .add(EntityAttributes.MAX_HEALTH, 80.0d)
             .add(EntityAttributes.MOVEMENT_SPEED, 0.25d)
             .add(EntityAttributes.ATTACK_DAMAGE, 6.0d);
+    };
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0, true)); // Attack targets
+        this.goalSelector.add(2, new ActiveTargetGoal<>(this,LivingEntity.class,10.0F,true,true)); // Follow targets
+        this.goalSelector.add(3, new WanderAroundFarGoal(this, 1.0)); // Wander behavior
+        this.goalSelector.add(4, new LookAroundGoal(this)); // Optional: look around randomly
     };
 };
