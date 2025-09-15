c05e222a54c47e51fc07aa3fb250705369bf5083
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-09-15 12:59:41 -0400

    superduperdev2

--- src/main/java/com/normalorg/bandw/Defiance.java
+++ src/main/java/com/normalorg/bandw/Defiance.java
@@ -23,7 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // MAIN MOD HANDLER AND INITIALIZER - For most things, this is the target initializer, If you need to register things, register them here.
-// -- Superduperdev2
+// --Superduperdev2
 public class Defiance implements ModInitializer {
     public static boolean isCharterLoaded;
     // Charter hooks
