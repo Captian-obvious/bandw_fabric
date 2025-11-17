a717684deca114a3b332ef11ecf63ec6c52dfe15
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-17 14:11:59 -0500

    fix animation for walker entity

--- src/client/java/com/normalorg/bandw/client/render/entity/state/WalkerEntityRenderState.java
+++ src/client/java/com/normalorg/bandw/client/render/entity/state/WalkerEntityRenderState.java
@@ -1,9 +1,9 @@
 package com.normalorg.bandw.client.render.entity.state;
 
 import com.normalorg.bandw.client.DefianceClient;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 
-public class WalkerEntityRenderState extends LivingEntityRenderState {
+public class WalkerEntityRenderState extends BipedEntityRenderState {
     public WalkerEntityRenderState() {
         super();
     };
