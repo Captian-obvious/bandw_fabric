e6ebeec4dcc0fb39df82fdf5a8ad1ce0a5790726
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-03 11:42:09 -0500

    divinity crosshair (no renderer yet)

---
+++ src/main/java/com/normalorg/bandw/entity/custom/DivinityCrosshairEntity.java
@@ -0,0 +1,22 @@
+package com.normalorg.bandw.entity.custom;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+
+public class DivinityCrosshairEntity extends HostileEntity { //this is odd but, lets us do things like spam damage
+    public DivinityCrosshairEntity(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    };
+    public static DefaultAttributeContainer.Builder createDivinityCrosshairAttributes() {
+        return HostileEntity.createHostileAttributes()
+            .add(EntityAttributes.MAX_HEALTH, 40.0D)
+            .add(EntityAttributes.MOVEMENT_SPEED, 0.25D)
+            .add(EntityAttributes.ATTACK_DAMAGE, 8.0D)
+            .add(EntityAttributes.FOLLOW_RANGE, 32.0D);
+    };
+};
