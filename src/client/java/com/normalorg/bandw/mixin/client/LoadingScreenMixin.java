5e6354b52df06250d73910bd13c3ee1b65fb8c19
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-06 22:34:14 -0500

    test epic equation

--- src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
+++ src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
@@ -25,7 +25,7 @@ public class LoadingScreenMixin {
         MinecraftClient client=MinecraftClient.getInstance();
         int screenWidth=client.getWindow().getScaledWidth();
         int screenHeight=client.getWindow().getScaledHeight();
-        float alpha=1.0f;
+        float alpha=(Math.sin(delta*Math.PI)+1.0f) / 2.0f;
         int baseColor=0xFF004F;
         int argbColor=((int)(alpha * 255) << 24) | baseColor;
     };
