3c8a95f399e242e774ec19cb6ce62d0e23448846
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-06 21:47:28 -0500

    attempt to silence warnings

--- src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
+++ src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.world.LevelLoadingScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
@@ -18,12 +19,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelLoadingScreen.class)
-public abstract class LoadingScreenMixin {
+public class LoadingScreenMixin {
     @Inject(method = "renderBackgroundTexture", at = @At("TAIL"), cancellable = true)
-    public static void onRenderBackgroundTexture(DrawContext context,Identifier texture,int x,int y,float u,float v,int width,int height,CallbackInfo ci){
+    private void onRenderBackgroundTexture(DrawContext context,Identifier texture,int x,int y,float u,float v,int width,int height,CallbackInfo ci){
         MinecraftClient client=MinecraftClient.getInstance();
         int screenWidth=client.getWindow().getScaledWidth();
         int screenHeight=client.getWindow().getScaledHeight();
-        
     };
 };
