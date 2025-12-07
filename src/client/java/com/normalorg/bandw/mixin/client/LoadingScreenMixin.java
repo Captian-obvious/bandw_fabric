3a36bbaa77301bbf7db82c9b457942769a3f3e18
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-06 22:17:29 -0500

    fix again

--- src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
+++ src/client/java/com/normalorg/bandw/mixin/client/LoadingScreenMixin.java
@@ -21,9 +21,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LevelLoadingScreen.class)
 public class LoadingScreenMixin {
     @Inject(method = "render", at = @At("TAIL"), cancellable = true)
-    public void render(DrawContext context,int mouseX,int mouseY,float delta, CallbackInfo ci){
+    public void onRender(DrawContext context,int mouseX,int mouseY,float delta, CallbackInfo ci){
         MinecraftClient client=MinecraftClient.getInstance();
         int screenWidth=client.getWindow().getScaledWidth();
         int screenHeight=client.getWindow().getScaledHeight();
+        float alpha=1.0f;
+        int baseColor=0xFF004F;
+        int argbColor=((int)(alpha * 255) << 24) | baseColor;
     };
 };
