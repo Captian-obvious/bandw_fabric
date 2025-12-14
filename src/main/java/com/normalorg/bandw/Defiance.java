106f2ef20cfd8d04478da3a315d6427d5a82e2d2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-14 17:19:56 -0500

    rebuild current mod version

--- src/main/java/com/normalorg/bandw/Defiance.java
+++ src/main/java/com/normalorg/bandw/Defiance.java
@@ -62,6 +62,7 @@ public class Defiance implements ModInitializer {
         ModPotions.registerPotions();
         ModEffects.registerEffects();
         ModSounds.registerSounds();
+        // gradle builder time
     };
     public static void registerCharterHooks(){
         LOGGER.info("Registering Charter Hooks (contract severing functionality)...");
