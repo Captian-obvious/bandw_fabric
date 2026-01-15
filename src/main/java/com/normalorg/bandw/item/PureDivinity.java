6e2a8edaf00e789f4c571c70489326992f5ca22e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-01-14 19:44:26 -0500

    h

--- src/main/java/com/normalorg/bandw/item/PureDivinity.java
+++ src/main/java/com/normalorg/bandw/item/PureDivinity.java
@@ -72,7 +72,7 @@ public class PureDivinity extends Item {
                 //power+=3.0F;
             };
             world.createExplosion(entity,null,null,pos.getX(),pos.getY(),pos.getZ(),power,true,World.ExplosionSourceType.NONE);
-            power=0.0F;
+            //power=0.0F;
             world.playSound(null,pos.getX(),pos.getY(),pos.getZ(),SoundEvents.BLOCK_BEACON_DEACTIVATE,SoundCategory.AMBIENT,1.0F,1.0F);
         };
         super.onItemEntityDestroyed(entity);
