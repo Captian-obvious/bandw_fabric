266cd1995e38481f48c3aaea2879395debe8bb64
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-01-21 11:44:48 -0500

    particles!

---
+++ src/main/java/com/normalorg/bandw/particle/ModParticles.java
@@ -0,0 +1,9 @@
+package com.normalorg.bandw.particle;
+
+import com.normalorg.bandw.Defiance;
+
+public class ModParticles {
+    public static void registerParticles() {
+        Defiance.LOGGER.info("Registering Mod Particles for " + Defiance.MOD_ID);
+    };
+};
