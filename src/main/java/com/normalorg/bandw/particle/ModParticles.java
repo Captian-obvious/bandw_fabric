425ec2cc75d45c38dc584d6bae3ba33ded9e459a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-01-21 11:49:30 -0500

    registries begin

--- src/main/java/com/normalorg/bandw/particle/ModParticles.java
+++ src/main/java/com/normalorg/bandw/particle/ModParticles.java
@@ -1,6 +1,11 @@
 package com.normalorg.bandw.particle;
 
 import com.normalorg.bandw.Defiance;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.SimpleParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModParticles {
     public static void registerParticles() {
