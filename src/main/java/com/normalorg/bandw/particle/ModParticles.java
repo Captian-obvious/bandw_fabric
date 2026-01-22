0d15291a4f9be40c5a609061afb50c6d1b745671
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-01-22 09:23:37 -0500

    test

--- src/main/java/com/normalorg/bandw/particle/ModParticles.java
+++ src/main/java/com/normalorg/bandw/particle/ModParticles.java
@@ -3,11 +3,17 @@ package com.normalorg.bandw.particle;
 import com.normalorg.bandw.Defiance;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.particle.SimpleParticleType;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModParticles {
+    public static final SimpleParticleType EXAMPLE_PARTICLE = register("example_particle", true);
+    public static SimpleParticleType register(String name,boolean alwaysShow) {
+        return Registry.register(Registries.PARTICLE_TYPE, Identifier.of(Defiance.MOD_ID, name), FabricParticleTypes.simple());
+    };
     public static void registerParticles() {
         Defiance.LOGGER.info("Registering Mod Particles for " + Defiance.MOD_ID);
     };
