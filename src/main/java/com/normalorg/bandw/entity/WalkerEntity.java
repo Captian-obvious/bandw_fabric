477cbb2df66965b4888b9cd35f82ec0ab86ff041
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-02-04 11:54:31 -0500

    attack goal

--- src/main/java/com/normalorg/bandw/entity/WalkerEntity.java
+++ src/main/java/com/normalorg/bandw/entity/WalkerEntity.java
@@ -24,9 +24,14 @@ public class WalkerEntity extends PathAwareEntity {
             .add(EntityAttributes.MOVEMENT_SPEED, 0.25d)
             .add(EntityAttributes.ATTACK_DAMAGE, 10.0d);
     };
+    public class WalkerAttackGoal extends MeleeAttackGoal {
+        public WalkerAttackGoal(PathAwareEntity mob, double speed, boolean pauseWhenMobIdle) {
+            super(mob, speed, pauseWhenMobIdle);
+        }
+    }
     @Override
     protected void initGoals() {
-        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0, true)); // Attack targets
+        this.goalSelector.add(1, new WalkerAttackGoal(this, 1.0, true)); // Attack targets
         this.goalSelector.add(2, new ActiveTargetGoal<LivingEntity>(this,LivingEntity.class,10,true,true,null)); // Follow targets
         this.goalSelector.add(3, new WanderAroundFarGoal(this, 1.0)); // Wander behavior
         this.goalSelector.add(4, new LookAroundGoal(this)); // Optional: look around randomly
