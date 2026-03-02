e65bb5ad9d3844c8ed783886b6fd89ec94b03f3b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-03-02 11:43:04 -0500

    effect

--- src/main/java/com/normalorg/bandw/entity/effect/ModEffects.java
+++ src/main/java/com/normalorg/bandw/entity/effect/ModEffects.java
@@ -14,11 +14,13 @@ public class ModEffects {
     public static final StatusEffect CLEANSING_EFFECT = new CleansingStatusEffect(StatusEffectCategory.BENEFICIAL, 0x111111);
     public static final StatusEffect HALFSHADE_POISONING_EFFECT = new HalfshadePoisoningStatusEffect(StatusEffectCategory.HARMFUL,0x9f9999);
     public static final StatusEffect CRYSTAL_INFESTATION_EFFECT = new CrystalInfestation(StatusEffectCategory.HARMFUL,0x10d6f0);
+    public static final StatusEffect FRACTURED_WEAKNESS_EFFECT = new FracturedWeakness(StatusEffectCategory.HARMFUL,0xaec6cf);
     public static RegistryEntry<StatusEffect> CORRUPTION;
     public static RegistryEntry<StatusEffect> DARKENING;
     public static RegistryEntry<StatusEffect> CLEANSING;
     public static RegistryEntry<StatusEffect> HALFSHADE_POISONING;
     public static RegistryEntry<StatusEffect> CRYSTAL_INFESTATION;
+    public static RegistryEntry<StatusEffect> FRACTURED_WEAKNESS;
     public static void registerEffects() {
         Defiance.LOGGER.info("Registering effects for " + Defiance.MOD_ID);
         CORRUPTION=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Defiance.MOD_ID, "corruption"), CORRUPTION_EFFECT);
@@ -26,5 +28,6 @@ public class ModEffects {
         CLEANSING=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Defiance.MOD_ID, "cleansing"), CLEANSING_EFFECT);
         HALFSHADE_POISONING=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Defiance.MOD_ID, "halfshade_poisoning"), HALFSHADE_POISONING_EFFECT);
         CRYSTAL_INFESTATION=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Defiance.MOD_ID, "crystal_infestation"), CRYSTAL_INFESTATION_EFFECT);
+        FRACTURED_WEAKNESS=Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(Defiance.MOD_ID, "fractured_weakness"), FRACTURED_WEAKNESS_EFFECT);
     };
 };
